aa583cab9f94849add0da451a18aa03a8eb88d7e
ZQL666-No1 <977139874@qq.com>
2020-10-23 17:47:52 +0800

    demo2

--- JS/ES6-class.html/JAVA/demo2.java
+++ JS/ES6-class.html/JAVA/demo2.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class demo2 {
     public static void main(String[] args) {
-        System.out.println("1232");
+        for (int i = 0; i < 39; i++) {
+            System.out.print(i + "  ");
+        }
+        int[] a = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            if ((100 % (i + 1)) == 0) {
+                a[i] = 100 * i;
+            } else {
+                a[i] = 56;
+            }
+        }
+        for (int j = 0; j < a.length; j++) {
+            System.out.print(a[j] + " ");
+        }
+        Scanner in = new Scanner(System.in);
+        int b = in.nextInt();
+
     }
 }
